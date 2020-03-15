8d49af0bfcf1db3ead55a6fd152584d96827d480
liximing <liximing127@gmail.com>
2020-03-15 17:40:41 +0900

    init

---
+++ src/main/java/com/johnson/mypagesgateway/MypagesGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.johnson.mypagesgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypagesGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypagesGatewayApplication.class, args);
+	}
+
+}
